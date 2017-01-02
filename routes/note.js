85e5f0ec89b27109944d088d35f196d87ca64949
iambrian7 <bkcgee@gmail.com>
2017-01-02 11:30:53 -0600

    added logs and dates

--- routes/note.js
+++ routes/note.js
@@ -6,14 +6,16 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Note = require('../models/noteModel')//(mongoose);
 router.post('/', function(req, res, next) {
+  console.dir('hostname: ' + req.hostname)
   var timestamp = new Date();
-
+ console.log('body=' + req.body)
     Note.create(
         {   name: req.body.name,
             title: req.body.title,
             comment: req.body.comment,
             tags: req.body.tags,
-          displayDate: timestamp.toLocaleDateString() + " " + timestamp.toLocaleTimeString()
+          // displayDate: timestamp.toLocaleDateString() + " " + timestamp.toLocaleTimeString()
+          displayDate: timestamp.toLocaleDateString() + " " + req.body.time
         }, function (err, note) {
             if (err) return handleError(err);
             // saved!
