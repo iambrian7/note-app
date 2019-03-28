f44c3fa3e5c71f9df1b3511036fd641d3a2522dd
iambrian7 <bkcgee@gmail.com>
2019-03-28 15:07:07 -0500

    Final changes for delete

--- routes/note.js
+++ routes/note.js
@@ -9,29 +9,13 @@ var Note = require('../models/noteModel')//(mongoose);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Note.find({}).sort({ dateNoteAdded : -1}).exec(function(err, notes){ //... });
- // Note.find().sort([['dateNoteAdded', 'descending']]).all(function (err, notes) {
-    // do something with the array of posts
-  // });
-  //   Note.find({},{
-  //     skip:0, // Starting Row
-  //     limit:10, // Ending Row
-  //     sort:{
-  //       dateNoteAdded: -1 //Sort by Date Added DESC
-  //     }
-  //   },function (err, notes) {
         if (err) {
-            console.log('found accounts error')
+            console.log('found notes error')
             return res.status(500).json({
                 message: 'Error when getting notes.',
                 error: err
             });
         }
-        //var text = text.replace(/&quot;/g, '\\"');  // fix the quote problem
-      //  console.log('listing found for notes')
-   // console.log('coment: ' + JSON.stringify(notes,null,4))//nodes)
-       // return res.json(notes);
-       // res.render('index', { title: 'Notes for Brian', notes: notes });
-       // res.json(200, notes);  //  express deprecated res.json(status, obj): Use res.status(status).json(obj) instead routes\note.js:34:13
         res.status(200).json(notes)
     });
 });
@@ -39,37 +23,62 @@ router.get('/', function(req, res, next) {
 router.post("/delete/:id", deleteUser); 
 
 function deleteUser(req,res){
-    console.log("deleting id= " + req.params.id)
+   // console.log("deleting id= " + req.params.id)
     Note.findById(req.params.id).remove().exec(function(err){
-        console.log("deleting id= " + req.params.id)
+      //  console.log("deleting id= " + req.params.id)
          if (!err) {
-                console.log('Removed Successfully');
+         //       console.log('Removed Successfully');
+         res.status(200).json({message:"success"})
         }
         else {
-                console.log('Error in removing the entry');
+        //        console.log('Error in removing the entry');
         }
     });
+  
 }
 
 router.post('/', function(req, res, next) {
  // console.dir('hostname: ' + req.hostname)
   var timestamp = new Date();
-// console.log('body=' + req.body.comment)
-  //var commentBreaks = req.body.comment.replace(/\n\r?/g, '<br />');
-    Note.create(
-        {   name: req.body.name,
-            title: req.body.title,
-          //  comment: commentBreaks, //req.body.comment,
-            comment: req.body.comment,
-            tags: req.body.tags,
-          // displayDate: timestamp.toLocaleDateString() + " " + timestamp.toLocaleTimeString()
-          displayDate: timestamp.toLocaleDateString() + " " + req.body.time
-        }, function (err, note) {
-            if (err) return handleError(err);
-            // saved!
-           // console.log('note saved with create: ' + JSON.stringify(note,null,4))
-        })
+ //console.log('body=' + JSON.stringify(req.body))
+ if (req.body.id ){ // if we have an id this is an update
+   // // This would likely be inside of a PUT request, since we're updating an existing document, hence the req.params.todoId.
+    // Find the existing resource by ID
+    Note.findByIdAndUpdate(
+      // the id of the item to find
+      req.body.id,
+      
+      // the change to be made. Mongoose will smartly combine your existing 
+      // document with this change, which allows for partial updates too
+      req.body,
+      
+      // an option that asks mongoose to return the updated version 
+      // of the document instead of the pre-updated one.
+      {new: true},
+      
+      // the callback function
+      (err, note) => {
+      // Handle any possible database errors
+          if (err) return res.status(500).send(err);
+      }
+    )
+  } else {
+   Note.create(
+     {   name: req.body.name,
+      title: req.body.title,
+      //  comment: commentBreaks, //req.body.comment,
+      comment: req.body.comment,
+      tags: req.body.tags,
+      // displayDate: timestamp.toLocaleDateString() + " " + timestamp.toLocaleTimeString()
+      displayDate: timestamp.toLocaleDateString() + " " + req.body.time
+    }, function (err, note) {
+      if (err) return handleError(err);
+      // saved!
+      // console.log('note saved with create: ' + JSON.stringify(note,null,4))
+    })
+  } // end of create
     res.redirect("/")
-});
-
-module.exports = router;
+  });
+  
+  module.exports = router;
+  
